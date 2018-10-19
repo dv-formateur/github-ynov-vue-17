674d2cdbdffb08087fe6af08dec9d46d9e59c654
mathiasLoiret <mathias44.loiret@hotmail.fr>
2018-10-19 11:30:52 +0200

    two parameters ok

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # github-ynov-vue
+
+1 : faire un appel à l'API 
+2 : lier mon résultat au boutons  
--- script/script.js
+++ script/script.js
@@ -1,12 +1,41 @@
-var maVoiture = {
-    fabricant : "Ford",
-    modèle : "Mustang",
-    année : 1969
-}
+var accounts = ["Killy85", "Mokui", "LordInateur", "gfourny", "ClementCaillaud",  "sfongue", "BenoitCochet", "Nair0fl", "raphaelCharre", "mathiasLoiret", "thomaspich", "TeofiloJ" ,"Grigusky" ,"mael61" ,"Dakistos", "KevinPautonnier", "alixnzt", "benjaminbra", "rudy8530", "AlexDesvallees", "Coblestone", "etienneYnov", "AntoineGOSSET" ];
+var projects = [];
 
-
-new Vue({
-    el: '#app',
-    data: maVoiture
+var app = new Vue({
+    el: '#acc',
+    data:{
+        accounts : [],
+        selected_account : null,
+        projects : [],
+        selected_project : null
+    }
 })
 
+function affiche(res){
+    res = JSON.parse(res);
+    res.forEach(obj => {
+        projects.push(obj.name);       
+    });
+    app.projects = projects;
+    app.accounts = accounts;
+}
+
+function getProject(theUrl, callback)
+{
+    var req = new XMLHttpRequest();
+    req.onreadystatechange = function() { 
+        if (req.status == 200){
+            callback(req.responseText);
+          }   
+        else{
+            console.log("Error");
+            console.log(req);
+        }
+    }
+    req.open("GET", theUrl, true); // true for asynchronous 
+    req.send();
+}
+
+getProject("https://api.github.com/users/killy85/repos?access_token=c452a730577fe6cdfd62dc0c51f5662a9e9f92b9", affiche);
+
+console.log(app.accounts);

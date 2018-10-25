019b822fdbd7dd191dc129349b5338e2272e3c45
mathiasLoiret <mathias44.loiret@hotmail.fr>
2018-10-25 08:10:02 +0200

    last

--- script/script.js
+++ script/script.js
@@ -1,5 +1,6 @@
 var accounts = ["Killy85", "Mokui", "LordInateur", "gfourny", "ClementCaillaud",  "sfongue", "BenoitCochet", "Nair0fl", "raphaelCharre", "mathiasLoiret", "thomaspich", "TeofiloJ" ,"Grigusky" ,"mael61" ,"Dakistos", "KevinPautonnier", "alixnzt", "benjaminbra", "rudy8530", "AlexDesvallees", "Coblestone", "etienneYnov", "AntoineGOSSET" ];
 var projects = [];
+var objects = [];
 
 var app = new Vue({
     el: '#acc',
@@ -7,20 +8,46 @@ var app = new Vue({
         accounts : [],
         selected_account : null,
         projects : [],
+        selected_project : null,
+        objects : [],
         selected_project : null
+    },
+    watch: {
+        selected_account : function (val){
+            getDatas("https://api.github.com/users/"+ val +"/repos?access_token=a5dafe9418fb8eebd6c54cbc6b4aaf3793ea5672", setProjects)
+        },
+        selected_project : function (val){
+            getDatas("https://api.github.com/repos/" + app.selected_account + "/" + val + "/commits?access_token=a5dafe9418fb8eebd6c54cbc6b4aaf3793ea5672", setCommits);
+        }
     }
 })
 
-function affiche(res){
+function onload(){
+    app.accounts = accounts;
+    app.projects = projects;
+    app.objects = objects;
+}
+
+function setProjects(res){
     res = JSON.parse(res);
     res.forEach(obj => {
         projects.push(obj.name);       
     });
     app.projects = projects;
-    app.accounts = accounts;
 }
 
-function getProject(theUrl, callback)
+function setCommits (res){
+    res = JSON.parse(res);
+    res.forEach(obj => {
+        objects.push(obj.commit);    
+    });
+    
+    app.objects = objects;
+    console.log(app.objects);
+    
+}
+
+function getDatas(theUrl, callback)
 {
     var req = new XMLHttpRequest();
     req.onreadystatechange = function() { 
@@ -35,7 +62,3 @@ function getProject(theUrl, callback)
     req.open("GET", theUrl, true); // true for asynchronous 
     req.send();
 }
-
-getProject("https://api.github.com/users/killy85/repos?access_token=c452a730577fe6cdfd62dc0c51f5662a9e9f92b9", affiche);
-
-console.log(app.accounts);
